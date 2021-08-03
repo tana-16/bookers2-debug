f8900b55ed3127083042bf387eced2201e7133f7
tana-16 <tanahiro1697@gmail.com>
2021-08-03 12:29:34 +0000

    [add]search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,45 +7,53 @@ class SearchesController < ApplicationController
   end
 
   private
-#   def search_for(model, content, method)
-#     if model == 'user'
-#       if method == 'perfect'
-#         User.where(name: content)
-#       else
-#         User.where('name LIKE ?', '%'+content+'%')
-#       end
-#     elsif model == 'book'
-#       if method == 'perfect'
-#         Book.where(title: content)
-#       else
-#         Post.where('title LIKE ?', '%'+content+'%')
-#       end
-#     end
-#   end
-
   def search_for(model, content, method)
     if model == 'user'
       if method == 'perfect'
         User.where(name: content)
       elsif model == "partial"
-        User.where('name LIKE ?', '%'+content+'%')
+        User.where('name LIKE ?', "%#{content}%")
       elsif model == "forward"
-        User.where('name LIKE ?', '%'+content+'%')
+        User.where('name LIKE ?', "#{content}%")
       else
-        User.where('name LIKE ?', '%'+content+'%')
+        User.where('name LIKE ?', "%#{content}")
       end
     elsif model == 'book'
       if method == 'perfect'
         Book.where(title: content)
       elsif model == "partial"
-        Book.where('title LIKE ?', '%'+content+'%')
+        Book.where('title LIKE ?', "%#{content}%")
       elsif model == "forward"
-        Book.where('title LIKE ?', '%'+content+'%')
+        Book.where('title LIKE ?', "#{content}%")
       else
-        Book.where('title LIKE ?', '%'+content+'%')
+        Book.where('title LIKE ?', "%#{content}")
       end
     end
   end
-  
+
+  # def search_for(model, content, method)
+  #   if model == 'user'
+  #     if method == 'perfect'
+  #       User.where(name: content)
+  #     elsif model == "partial"
+  #       User.where('name LIKE ?', '%'+content+'%')
+  #     elsif model == "forward"
+  #       User.where('name LIKE ?', '%'+content+'%')
+  #     else
+  #       User.where('name LIKE ?', '%'+content+'%')
+  #     end
+  #   elsif model == 'book'
+  #     if method == 'perfect'
+  #       Book.where(title: content)
+  #     elsif model == "partial"
+  #       Book.where('title LIKE ?', '%'+content+'%')
+  #     elsif model == "forward"
+  #       Book.where('title LIKE ?', '%'+content+'%')
+  #     else
+  #       Book.where('title LIKE ?', '%'+content+'%')
+  #     end
+  #   end
+  # end
+
 end
 
